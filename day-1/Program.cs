a2b8a8aa7068cea1f7b9a59d68ad56ed3840a081
csg <cgrigore@gmail.com>
2020-12-02 19:28:14 +0200

    day 1

---
+++ day-1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Xunit;
+using System.Linq;
+
+namespace day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = File.ReadAllLines("input.txt").Select(o => int.Parse(o)).ToArray();
+
+            Assert.Equal(514579, find(new int[]{1721,979,366,299,675,1456}));
+           System.Console.WriteLine(find(numbers));//1009899
+
+            Assert.Equal(241861950, find3(new int[]{1721,979,366,299,675,1456}));
+            System.Console.WriteLine(find3(numbers));//44211152
+
+        }
+
+       static int find(int[] a){
+            for(int i=0;i<a.Length;i++)
+                for(int j=i+1;j<a.Length;j++)
+                    if(a[i]+a[j]==2020)
+                        return a[i]*a[j];
+            
+            throw new Exception("not found");
+        }
+
+        static int find3(int[] a){
+            for(int i=0;i<a.Length;i++)
+                for(int j=i+1;j<a.Length;j++)
+                    for(int k=j+1;k<a.Length;k++)
+                    if(a[i]+a[j]+a[k]==2020)
+                        return a[i]*a[j]*a[k];
+
+            throw new Exception("not found");
+        }
+    }
+}
