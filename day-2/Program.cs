cea5e4739f12bf5712af750c729912541cac0c5f
csg <cgrigore@gmail.com>
2020-12-02 20:15:49 +0200

    first problem

---
+++ day-2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using Xunit;
+using System.Linq;
+
+namespace day_2
+{
+    class Program
+    {
+        struct policy{
+            public int min;
+            public int max;
+            public string letter;//todo
+            public string password;
+        }
+        
+        static policy topolicy(string l){
+            policy pol=new policy();
+            var pol_and_pass=l.Split(": ");
+            var int_and_letter=pol_and_pass[0].Split(" ");
+            var min_and_max=int_and_letter[0].Split("-");
+            pol.min=int.Parse(min_and_max[0]);
+            pol.max=int.Parse(min_and_max[1]);
+            pol.letter=int_and_letter[1];
+            pol.password=pol_and_pass[1];
+            return pol;
+ 
+        }
+
+        static void Main(string[] args)
+        {
+            var numbers_sample = File.ReadAllLines("sample.txt").Select(o => topolicy(o)).ToArray();
+            var numbers = File.ReadAllLines("input.txt").Select(o => topolicy(o)).ToArray();
+  
+            Assert.Equal(2, compute(numbers_sample));
+            Assert.Equal(467, compute(numbers));
+            
+            
+
+
+        }
+
+       static int compute(policy[] ps){
+           int cnt=0;
+           foreach(var p in ps){
+               var newp=p.password.Replace(p.letter,"");
+               var oc=p.password.Length-newp.Length;
+               if(p.min<=oc&&oc<=p.max)
+                cnt++;
+           }
+
+           return cnt;
+            
+        }
+    }
+}
