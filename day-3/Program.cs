426c5163cd986fca194946518d7adafcefac4c1b
csg <cgrigore@gmail.com>
2020-12-03 07:40:31 +0200

    1st problem

---
+++ day-3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using Xunit;
+using System.Linq;
+
+namespace day_3
+{
+    class Program
+    {
+
+
+        static char[][] readmatrix(string[] input)
+        {
+            char[][] matrix = new char[input.Length][];
+            for (int i = 0; i < input.Length; i++)
+            {
+                matrix[i] = new char[input[i].Length];
+                for (int j = 0; j < input[i].Length; j++)
+                    matrix[i][j] = input[i][j];
+            }
+            matrix.ShowMatrix();
+            return matrix;
+        }
+        static void Main(string[] args)
+        {
+
+
+            Compute(readmatrix(File.ReadAllLines("sample.txt")));//7
+            Compute(readmatrix(File.ReadAllLines("input.txt")));//292
+
+
+        }
+
+        static int Compute(char[][] matrix)
+        {
+            var ilen = matrix.Length;
+            var jlen = matrix[0].Length;
+
+            var i = 0;
+            var j = 0;
+            var cnt = 0;
+
+            do
+            {
+                System.Console.WriteLine($"{i} {j}");
+                i += 1;
+                j += 3;
+                // if (j >= jlen)
+                {
+                    j = j % jlen;
+                    // i = 0;
+                }
+                if (matrix[i][j] == '#')
+                    cnt++;
+
+            } while (i != ilen - 1);
+
+            System.Console.WriteLine(cnt);
+            return cnt;
+        }
+    }
+}
---
+++ day-3/Utils.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using static System.String;
+using System.Linq;
+
+namespace day_3
+{
+    public static class Utils
+    {
+
+
+        public static void ShowMatrix(this char[][] img, int lmax, int cmax)
+        {
+            for (int l = 0; l < lmax; l++)
+            {
+                for (int c = 0; c < cmax; c++)
+                {
+                    System.Console.Write(img[l][c]);
+                    // if (img[l][c] == '0')
+                    //     Console.Write(' ');
+                    // else
+                    //     Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void ShowMatrix(this char[][] img)
+        {
+            img.ShowMatrix(img.Length, img[0].Length);
+
+        }
+        // public static void DrawMatrix(this char[][] img, int lmax, int cmax)
+        // {
+        //     using (var bmp = new Bitmap(cmax, lmax))
+        //     using (var gr = Graphics.FromImage(bmp))
+        //     {
+        //         for (int l = 0; l < lmax; l++)
+        //         {
+        //             for (int c = 0; c < cmax; c++)
+        //             {
+        //                 bmp.SetPixel(c, l, img[l][c] == '1' ? Color.Black : Color.White);
+        //             }
+        //         }
+        //         var path = "out.png";
+        //         bmp.Save(path);
+        //     }
+        // }
+
+        public static string Combine(this char[][] img)
+        {
+            return Join("", img.Select(o => Join("", o)));
+        }
+    }
+}
