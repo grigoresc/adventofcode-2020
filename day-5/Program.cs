d94d4746a2492d344eba26cdf65bfcf4e079e63f
csg <cgrigore@gmail.com>
2020-12-05 01:10:10 +0200

    prepare day 5

---
+++ day-5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Xunit;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace day_5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Solve1(File.ReadAllText("sample.txt"));
+            //Assert.Equal(2, Solve1(File.ReadAllText("sample.txt")));
+        }
+
+        private static int Solve1(string str)
+        {
+            var ps = str.Split("\n\n");
+            var cnt = 0;
+
+            System.Console.WriteLine(cnt);
+            return cnt;
+        }
+
+
+    }
+}
