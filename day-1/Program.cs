d246c34cdfbc075a9d1e0a3f5b364d256af9b765
csg <cgrigore@gmail.com>
2020-12-05 01:21:02 +0200

    adjust day 1

--- day-1/Program.cs
+++ day-1/Program.cs
@@ -12,10 +12,10 @@ namespace day_1
             var numbers = File.ReadAllLines("input.txt").Select(o => int.Parse(o)).ToArray();
 
             Assert.Equal(514579, find(new int[] { 1721, 979, 366, 299, 675, 1456 }));
-            System.Console.WriteLine(find(numbers));//1009899
+            Assert.Equal(1009899, find(numbers));
 
             Assert.Equal(241861950, find3(new int[] { 1721, 979, 366, 299, 675, 1456 }));
-            System.Console.WriteLine(find3(numbers));//44211152
+            Assert.Equal(44211152, find3(numbers));
 
         }
 
@@ -24,7 +24,11 @@ namespace day_1
             for (int i = 0; i < a.Length; i++)
                 for (int j = i + 1; j < a.Length; j++)
                     if (a[i] + a[j] == 2020)
-                        return a[i] * a[j];
+                    {
+                        var ret = a[i] * a[j];
+                        System.Console.WriteLine(ret);
+                        return ret;
+                    }
 
             throw new Exception("not found");
         }
@@ -35,8 +39,11 @@ namespace day_1
                 for (int j = i + 1; j < a.Length; j++)
                     for (int k = j + 1; k < a.Length; k++)
                         if (a[i] + a[j] + a[k] == 2020)
-                            return a[i] * a[j] * a[k];
-
+                        {
+                            var ret = a[i] * a[j] * a[k];
+                            System.Console.WriteLine(ret);
+                            return ret;
+                        }
             throw new Exception("not found");
         }
     }
