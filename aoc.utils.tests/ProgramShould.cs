f7fedb816b0904775f133b5eeb499bc1124150bf
ciprian grigore <stefan.grigore@maxcode.net>
2020-12-06 22:43:15 +0200

    prepare day 7

--- aoc.utils.tests/ProgramShould.cs
+++ aoc.utils.tests/ProgramShould.cs
@@ -5,15 +5,20 @@ namespace aoc.utils.tests
     public class ProgramShould
     {
         [Fact]
-        public void SolveDay6()
-        {
-            day_6.Program.Solve();
-        }
-
-        [Fact]
         public void SolveDay2()
         {
             day_2.Program.Solve();
         }
+        [Fact]
+        public void SolveDay6()
+        {
+            day_6.Program.Solve();
+        }
+        [Fact]
+        public void SolveDay7()
+        {
+            day_7.Program.Solve();
+        }
+
     }
 }
---
+++ day-7/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace day_7
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Solve();
+        }
+
+        static public void Solve()
+        {
+            aoc.utils.Log.WriteLine("");
+            Assert.Equal(11, Solve1(File.ReadAllText("day-7.sample.txt")));
+            Assert.Equal(6382, Solve1(File.ReadAllText("day-7.input.txt")));
+            Assert.Equal(6, Solve2(File.ReadAllText("day-7.sample.txt")));
+            Assert.Equal(3197, Solve2(File.ReadAllText("day-7.input.txt")));
+        }
+
+        static int Solve1(string input)
+        {
+            var a = input.Split("\r\n\r\n");
+            aoc.utils.Log.WriteLine(a.Length);
+
+            var ret = a.Select(g =>
+            {
+                var all = g.Replace("\r\n", "");
+                var cnt = all.Distinct<char>().Count();
+                return cnt;
+            }).Sum();
+
+            aoc.utils.Log.WriteLine($"ret:{ret}");
+            return ret;
+        }
+
+        static int Solve2(string input)
+        {
+
+            var a = input.Split("\r\n\r\n");
+            aoc.utils.Log.WriteLine(a.Length);
+
+            var ret = a.Select(g =>
+              {
+                  var pers = g.Split("\r\n");
+                  var first = pers[0];
+
+                  var all = g.Replace("\r\n", "");
+                  var cnt = 0;
+                  foreach (var a in first)
+                  {
+                      if (all.Count(c => c == a) == pers.Length)
+                          cnt++;
+                  }
+
+                  return cnt;
+              }).Sum();
+
+            aoc.utils.Log.WriteLine($"ret:{ret}");
+            return ret;
+        }
+    }
+}
