63182de0ed7b09ba19fa90115caf67bf12696b3c
ciprian grigore <stefan.grigore@maxcode.net>
2020-12-07 22:48:49 +0200

    simplify day7

--- day-7/Program.cs
+++ day-7/Program.cs
@@ -2,13 +2,12 @@
 using System.Linq;
 using Xunit;
 using aoc.utils;
-using System.Collections;
 using System.Collections.Generic;
 using System;
 
 namespace day_7
 {
-    struct bag
+    struct Bag
     {
         public string name;
         public IList<int> cnt;
@@ -24,49 +23,27 @@ namespace day_7
 
         static public void Solve()
         {
-            aoc.utils.Log.WriteLine("-------------------------------------");
             Assert.Equal(4, Solve1(File.ReadAllText("day-7.sample.txt")));
-            //Assert.Equal(302, Solve1(File.ReadAllText("day-7.input.txt")));
+            Assert.Equal(302, Solve1(File.ReadAllText("day-7.input.txt")));
 
-            Assert.Equal(32, Solve2(File.ReadAllText("day-7.sample.txt")));//32,126,4165
-            Assert.Equal(126, Solve2(File.ReadAllText("day-7.sample2.txt")));//32,126,4165
-            Assert.Equal(4165, Solve2(File.ReadAllText("day-7.input.txt")));//32,126,4165
+            Assert.Equal(32, Solve2(File.ReadAllText("day-7.sample.txt")));
+            Assert.Equal(126, Solve2(File.ReadAllText("day-7.sample2.txt")));
+            Assert.Equal(4165, Solve2(File.ReadAllText("day-7.input.txt")));
 
         }
 
-        static int Solve1(string input)
-        {
-            var all = Parse(input);
-
-            var src = "shiny gold bags";
-
-            var ret = all.Where(o =>
-            {
-                return Contain(1, o.Value, all, src);
-
-            }).Count();
-
-            aoc.utils.Log.WriteLine($"ret:{ret}");
-
-            return ret;
-        }
-
-        private static Dictionary<string, bag> Parse(string input)
+        private static Dictionary<string, Bag> Parse(string input)
         {
             var a = input.Split("\r\n");
-            aoc.utils.Log.WriteLine(a.Length);
 
             var all = a.Select(g =>
             {
-
-
                 var (bn, bags, _) = g.Split(" contain ");
                 var lb = bags.Substring(0, bags.Length - 1).Split(", ");
                 var lbs = lb.Select(o =>
                 {
                     if (o.EndsWith("bag"))
                         o += "s";
-
 
                     var (no, _) = o.Split(" ");
                     var name = o.Replace(no + " ", "");
@@ -76,7 +53,7 @@ namespace day_7
                         return (int.Parse(no), name);
                 }).ToList();
 
-                bag b;
+                Bag b;
                 b.name = bn;
                 b.inside = lbs.Select(o => o.Item2).ToList();
                 b.cnt = lbs.Select(o => o.Item1).ToList();
@@ -86,45 +63,56 @@ namespace day_7
 
             return all;
         }
+        static int Solve1(string input)
+        {
+            var all = Parse(input);
 
+            var src = "shiny gold bags";
+
+            var ret = all.Where(o =>
+            {
+                return Contain(o.Value, all, src);
+
+            }).Count();
+
+            Console.WriteLine(ret);
+            return ret;
+        }
         static int Solve2(string input)
         {
             var all = Parse(input);
 
             var src = "shiny gold bags";
-            var ret = Sum(1, all[src], all, src);
+            var ret = Sum(all[src], all);
             ret -= 1;
-            aoc.utils.Log.WriteLine($"ret: {ret}");
 
+            Console.WriteLine(ret);
             return ret;
         }
 
-        private static int Sum(int lev, bag o, Dictionary<string, bag> all, string src)
+        private static int Sum(Bag o, Dictionary<string, Bag> all)
         {
-            Console.WriteLine(lev + " " + o.name);
             var cnt = 0;
             for (int i = 0; i < o.inside.Count; i++)
             {
-
                 var cb = o.inside[i];
                 if (all.ContainsKey(cb))
                 {
                     var chi = all[cb];
-                    cnt += o.cnt[i] * Sum(lev + 1, chi, all, src);
+                    cnt += o.cnt[i] * Sum(chi, all);
                 }
             }
             return cnt + 1;
         }
 
-        private static bool Contain(int lev, bag o, Dictionary<string, bag> all, string src)
+        private static bool Contain(Bag o, Dictionary<string, Bag> all, string src)
         {
-            Console.WriteLine(lev + " " + o.name);
             foreach (var cb in o.inside)
             {
                 if (all.ContainsKey(cb))
                 {
                     var chi = all[cb];
-                    if (Contain(lev + 1, chi, all, src))
+                    if (Contain(chi, all, src))
                         return true;
                 }
             }
