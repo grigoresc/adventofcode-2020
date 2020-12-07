39c08043a26b9fe53e602eca194336ac0510c245
ciprian grigore <stefan.grigore@maxcode.net>
2020-12-07 09:51:39 +0200

    2nd problem solved

--- day-7/Program.cs
+++ day-7/Program.cs
@@ -11,6 +11,7 @@ namespace day_7
     struct bag
     {
         public string name;
+        public IList<int> cnt;
         public IList<string> inside;
     }
     public class Program
@@ -24,38 +25,12 @@ namespace day_7
         {
             aoc.utils.Log.WriteLine("-------------------------------------");
 
-            Solve2(File.ReadAllText("day-7.input.txt"));
+            Solve2(File.ReadAllText("day-7.input.txt"));//32,126,4165
         }
 
         static int Solve1(string input)
         {
-            var a = input.Split("\r\n");
-            aoc.utils.Log.WriteLine(a.Length);
-
-            var all = a.Select(g =>
-            {
-
-
-                var (bn, bags, _) = g.Split(" contain ");
-                var lb = bags.Substring(0, bags.Length - 1).Split(", ");
-                var lbs = lb.Select(o =>
-                {
-                    if (o.EndsWith("bag"))
-                        o += "s";
-
-
-                    var (no, _) = o.Split(" ");
-                    var name = o.Replace(no + " ", "");
-                    //Log.WriteLine(name);
-                    return name;
-                }).ToList();
-
-                bag b;
-                b.name = bn;
-                b.inside = lbs;
-
-                return new Tuple<string, bag>(bn, b);
-            });
+            IEnumerable<Tuple<string, bag>> all = Parse(input);
 
             var src = "shiny gold bags";
 
@@ -69,7 +44,8 @@ namespace day_7
 
             return all.Count();
         }
-        static int Solve2(string input)
+
+        private static IEnumerable<Tuple<string, bag>> Parse(string input)
         {
             var a = input.Split("\r\n");
             aoc.utils.Log.WriteLine(a.Length);
@@ -89,27 +65,47 @@ namespace day_7
                     var (no, _) = o.Split(" ");
                     var name = o.Replace(no + " ", "");
                     //Log.WriteLine(name);
-                    return name;
+                    if (no == "no")
+                        return (0, "No");
+                    else
+                        return (int.Parse(no), name);
                 }).ToList();
 
                 bag b;
                 b.name = bn;
-                b.inside = lbs;
+                b.inside = lbs.Select(o => o.Item2).ToList();
+                b.cnt = lbs.Select(o => o.Item1).ToList();
 
                 return new Tuple<string, bag>(bn, b);
             });
+            return all;
+        }
+
+        static int Solve2(string input)
+        {
+            IEnumerable<Tuple<string, bag>> all = Parse(input);
 
             var src = "shiny gold bags";
-
-            var ret = all.Where(o =>
-            {
-                return Contain(1, o, all, src);
-
-            }).Count();
-
-            aoc.utils.Log.WriteLine($"ret:{ret}");
+            var ret = Sum(1, all.FirstOrDefault(x => x.Item2.name == src), all, src);
+            ret -= 1;
+            aoc.utils.Log.WriteLine($"ret: {ret}");
 
             return all.Count();
+        }
+
+        private static int Sum(int lev, Tuple<string, bag> o, IEnumerable<Tuple<string, bag>> all, string src)
+        {
+            Console.WriteLine(lev + " " + o.Item1);
+            var cnt = 0;
+            for (int i = 0; i < o.Item2.inside.Count; i++)
+            {
+
+                var chi = all.FirstOrDefault(x => x.Item1 == o.Item2.inside[i]);
+
+                if (chi != null)
+                    cnt += o.Item2.cnt[i] * Sum(lev + 1, chi, all, src);
+            }
+            return cnt + 1;
         }
 
         private static bool Contain(int lev, Tuple<string, bag> o, IEnumerable<Tuple<string, bag>> all, string src)
