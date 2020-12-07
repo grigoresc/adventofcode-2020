cc2684654bc36835bb584de7213f243c2663aaf7
ciprian grigore <stefan.grigore@maxcode.net>
2020-12-07 22:30:58 +0200

    use dictionary

--- day-7/Program.cs
+++ day-7/Program.cs
@@ -36,13 +36,13 @@ namespace day_7
 
         static int Solve1(string input)
         {
-            IEnumerable<Tuple<string, bag>> all = Parse(input);
+            var all = Parse(input);
 
             var src = "shiny gold bags";
 
             var ret = all.Where(o =>
             {
-                return Contain(1, o, all, src);
+                return Contain(1, o.Value, all, src);
 
             }).Count();
 
@@ -51,7 +51,7 @@ namespace day_7
             return ret;
         }
 
-        private static IEnumerable<Tuple<string, bag>> Parse(string input)
+        private static Dictionary<string, bag> Parse(string input)
         {
             var a = input.Split("\r\n");
             aoc.utils.Log.WriteLine(a.Length);
@@ -70,7 +70,6 @@ namespace day_7
 
                     var (no, _) = o.Split(" ");
                     var name = o.Replace(no + " ", "");
-                    //Log.WriteLine(name);
                     if (no == "no")
                         return (0, "No");
                     else
@@ -82,49 +81,54 @@ namespace day_7
                 b.inside = lbs.Select(o => o.Item2).ToList();
                 b.cnt = lbs.Select(o => o.Item1).ToList();
 
-                return new Tuple<string, bag>(bn, b);
-            });
+                return b;
+            }).ToDictionary(o => o.name);
+
             return all;
         }
 
         static int Solve2(string input)
         {
-            IEnumerable<Tuple<string, bag>> all = Parse(input);
+            var all = Parse(input);
 
             var src = "shiny gold bags";
-            var ret = Sum(1, all.FirstOrDefault(x => x.Item2.name == src), all, src);
+            var ret = Sum(1, all[src], all, src);
             ret -= 1;
             aoc.utils.Log.WriteLine($"ret: {ret}");
 
             return ret;
         }
 
-        private static int Sum(int lev, Tuple<string, bag> o, IEnumerable<Tuple<string, bag>> all, string src)
+        private static int Sum(int lev, bag o, Dictionary<string, bag> all, string src)
         {
-            Console.WriteLine(lev + " " + o.Item1);
+            Console.WriteLine(lev + " " + o.name);
             var cnt = 0;
-            for (int i = 0; i < o.Item2.inside.Count; i++)
+            for (int i = 0; i < o.inside.Count; i++)
             {
 
-                var chi = all.FirstOrDefault(x => x.Item1 == o.Item2.inside[i]);
-
-                if (chi != null)
-                    cnt += o.Item2.cnt[i] * Sum(lev + 1, chi, all, src);
+                var cb = o.inside[i];
+                if (all.ContainsKey(cb))
+                {
+                    var chi = all[cb];
+                    cnt += o.cnt[i] * Sum(lev + 1, chi, all, src);
+                }
             }
             return cnt + 1;
         }
 
-        private static bool Contain(int lev, Tuple<string, bag> o, IEnumerable<Tuple<string, bag>> all, string src)
+        private static bool Contain(int lev, bag o, Dictionary<string, bag> all, string src)
         {
-            Console.WriteLine(lev + " " + o.Item1);
-            foreach (var co in o.Item2.inside)
+            Console.WriteLine(lev + " " + o.name);
+            foreach (var cb in o.inside)
             {
-                var chi = all.FirstOrDefault(o => o.Item1 == co);
-                if (chi != null)
+                if (all.ContainsKey(cb))
+                {
+                    var chi = all[cb];
                     if (Contain(lev + 1, chi, all, src))
                         return true;
+                }
             }
-            return o.Item2.inside.Where(o => o == src).Count() > 0;
+            return o.inside.Where(o => o == src).Count() > 0;
         }
     }
 }
