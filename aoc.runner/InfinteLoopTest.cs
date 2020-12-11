fcf41d550466ad1e247c95d4bf9bfccf819f2825
ciprian grigore <stefan.grigore@maxcode.net>
2020-12-11 06:59:32 +0200

    prepare day 11

--- aoc.runner/InfinteLoopTest.cs
+++ aoc.runner/InfinteLoopTest.cs
@@ -15,7 +15,7 @@ namespace aoc.runner
             writer.AutoFlush = true;
 
             Console.WriteLine($"RUN BEGIN {DateTime.Now}");
-            day_10.Program.Solve();
+            day_11.Program.Solve();
             Console.WriteLine($"RUN END {DateTime.Now}");
         }
     }
---
+++ day-11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Linq;
+using Xunit;
+using aoc.utils;
+using System.Collections.Generic;
+using System;
+
+namespace day_11
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Solve();
+        }
+        public static void Solve()
+        {
+            Console.WriteLine(Solve1(File.ReadAllLines("day-10.input.txt")));
+            Console.WriteLine(Solve2(File.ReadAllLines("day-10.input.txt")));
+        }
+
+        static long Solve1(string[] lines)
+        {
+            long ret = 0;
+            return ret;
+        }
+
+
+        static long Solve2(string[] lines)
+        {
+            long ret = 0;
+            return ret;
+        }
+    }
+}
