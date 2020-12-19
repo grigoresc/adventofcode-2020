1f0f1f3e8407633b0ba26c425d8264811c09c358
csg <cgrigore@gmail.com>
2020-12-19 23:08:53 +0200

    prepare day 20

---
+++ day-20/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Linq;
+using Xunit;
+using aoc.utils;
+using System.Collections.Generic;
+using System;
+using System.Text.RegularExpressions;
+
+namespace day_20
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Solve();
+        }
+
+        public static void Solve()
+        {
+            Solve1(File.ReadAllText("day-20.sample.txt"));
+        }
+
+        private static long Solve1(string input)
+        {
+            long ret = 0L;
+            Console.WriteLine(ret);
+            return ret;
+        }
+
+    }
+
+}
