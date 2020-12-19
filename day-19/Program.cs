b64e0884dbeecf5a06782945c7219c81e3c8eef9
ciprian grigore <stefan.grigore@maxcode.net>
2020-12-19 13:54:35 +0200

    1st problem still working with regexes, however I dont think it will work for 2nd problem

--- day-19/Program.cs
+++ day-19/Program.cs
@@ -4,6 +4,7 @@ using Xunit;
 using aoc.utils;
 using System.Collections.Generic;
 using System;
+using System.Text.RegularExpressions;
 
 namespace day_19
 {
@@ -13,6 +14,12 @@ namespace day_19
         static void Main(string[] args)
         {
             Solve();
+            var e = buildreg("ceva", "bun", 2);
+            Regex r = new Regex(e);
+            var m1 = r.Match("cevabun");
+            m1 = r.Match("cevacevabun");
+            m1 = r.Match("cevacevabun");
+            m1 = r.Match("cevacevacevabun");
         }
 
         public static void Solve()
@@ -21,6 +28,10 @@ namespace day_19
             //solve1(File.ReadAllText("day-19.sample2.p2.txt"));
         }
 
+        static string buildreg(string s42, string s31, int reps)
+        {
+            return $"^({s42}){{1,}}({s42}){{{reps}}}({s31}){{{reps}}}$";
+        }
         private static long solve1(string input)
         {
             long ret;
@@ -28,6 +39,14 @@ namespace day_19
             string[] li;
             Parse(input, out rules, out li);
             ret = 0L;
+
+            var liminle = li.Select(l => l.Length).Min();
+            var limaxle = li.Select(l => l.Length).Max();
+            var limi = li.Where(l => l.Length == liminle).Count();
+            var linonmi = li.Where(l => l.Length > liminle).Count();
+
+            //var line = Convert.ToInt32(li[0].Substring(0, 8).Replace("b", "1").Replace("a", "1"), 2);
+
             //var all = construct(rules, 0);
             var all42 = Construct(rules, 42);
             var all31 = Construct(rules, 31);
@@ -35,18 +54,40 @@ namespace day_19
             //all = new[] { new[] { 11L } };
             all = Combine(all42, all31);
             all = Combine(all42, all);
+            var all42strings = ExtractStrings(rules, all42);
+            var all31strings = ExtractStrings(rules, all31);
+            var all42strings_max = all42strings.Select(l => l.Length).Max();
+            var all31strings_max = all31strings.Select(l => l.Length).Max();
 
             //var c8 = all.Where((m, idx) => m.Skip(1).Contains(8)).ToArray();
             //var c11 = all.Where((m, idx) => m.Where(o => o == 11).Count() > 1).ToArray();
-            string[] allstrings = ExtractStrings(rules, all);
+            //string[] allstrings = ExtractStrings(rules, all);
+            string[] allstrings = CombineAndRegex(all42strings, all31strings, (a, b) =>
+                     {
+                         return $"({a}){{1}}({b}){{1}}";
+                     }
+                );
+            allstrings = CombineAndRegex(all42strings, allstrings, (a, b) =>
+                     {
+                         return $"^({a}){{1}}({b}){{1}}$";
+                     }
+                );
 
-            var m = li.Where(l => allstrings.Contains(l));
+            var sln = new List<int>();
+            foreach (var o in allstrings)
+            {
+                var r = new Regex(o);
+                var f = li
+                            .Select((o, idx) => (o, idx))
+                            .Where((s, idx) => !sln.Contains(idx) && r.IsMatch(s.o))
+                            .Select(o => o.idx).ToList();
+                if (f.Count() > 0)
+                    sln.AddRange(f);
+            };
 
-            var liminle = li.Select(l => l.Length).Min();
-            var limaxle = li.Select(l => l.Length).Max();
-            var limi = li.Where(l => l.Length == liminle).Count();
-            var linonmi = li.Where(l => l.Length > liminle).Count();
-            ret = m.Count();
+            //var m = li.Where(l => .FirstOrDefault(r => r.IsMatch(l)) != null);
+
+            ret = sln.Count();
 
             Console.WriteLine(ret);
             return ret;
@@ -125,6 +166,13 @@ namespace day_19
             s = (from first in s
                  from second in li
                  select first.Concat(second).ToArray()).ToArray();
+            return s;
+        }
+        private static string[] CombineAndRegex(string[] s, string[] li, Func<string, string, string> applyRegex)
+        {
+            s = (from first in s
+                 from second in li
+                 select applyRegex(first, second)).ToArray();
             return s;
         }
     }
