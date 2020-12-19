0becc234c86a0a38f9de31a6078d7300ecc98558
ciprian grigore <stefan.grigore@maxcode.net>
2020-12-19 14:45:36 +0200

    solved 2nd problem

--- day-19/Program.cs
+++ day-19/Program.cs
@@ -45,41 +45,21 @@ namespace day_19
             var limi = li.Where(l => l.Length == liminle).Count();
             var linonmi = li.Where(l => l.Length > liminle).Count();
 
-            //var line = Convert.ToInt32(li[0].Substring(0, 8).Replace("b", "1").Replace("a", "1"), 2);
-
-            //var all = construct(rules, 0);
-            var all42 = Construct(rules, 42);
 
             var all42_reg = ConstructRegex(rules, 42);
             var all31_reg = ConstructRegex(rules, 31);
 
-            var all31 = Construct(rules, 31);
-            long[][] all;
-            //all = new[] { new[] { 11L } };
-            all = Combine(all42, all31);
-            all = Combine(all42, all);
-            var all42strings = ExtractStrings(rules, all42);
-            var all31strings = ExtractStrings(rules, all31);
-            var all42strings_max = all42strings.Select(l => l.Length).Max();
-            var all31strings_max = all31strings.Select(l => l.Length).Max();
+            //12 chunks max; reg has 1+2 min;
+            var allstrings_reg_a = new List<string>();
 
-            //var c8 = all.Where((m, idx) => m.Skip(1).Contains(8)).ToArray();
-            //var c11 = all.Where((m, idx) => m.Where(o => o == 11).Count() > 1).ToArray();
-            //string[] allstrings = ExtractStrings(rules, all);
-            string[] allstrings = CombineAndRegex(all42strings, all31strings, (a, b) =>
-                     {
-                         return $"({a}){{1}}({b}){{1}}";
-                     }
-                );
-            allstrings = CombineAndRegex(all42strings, allstrings, (a, b) =>
-                     {
-                         return $"^({a}){{1}}({b}){{1}}$";
-                     }
-                );
-
-            var allstrings_reg = $"^({all42_reg}){{1}}({all42_reg}){{1}}({all31_reg}{{1}})$";
-
-            var allstrings_reg_a = new string[] { allstrings_reg };
+            for (var rleft = 1; rleft <= 10; rleft++)
+            {
+                for (var rright = 1; rright <= 5; rright++)
+                {
+                    var allstrings_reg = $"^({all42_reg}){{{rleft}}}({all42_reg}){{{rright}}}({all31_reg}{{{rright}}})$";
+                    allstrings_reg_a.Add(allstrings_reg);
+                }
+            }
             var sln = new List<int>();
             foreach (var o in allstrings_reg_a)
             {
@@ -91,8 +71,6 @@ namespace day_19
                 if (f.Count() > 0)
                     sln.AddRange(f);
             };
-
-            //var m = li.Where(l => .FirstOrDefault(r => r.IsMatch(l)) != null);
 
             ret = sln.Count();
 
