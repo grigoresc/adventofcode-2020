60d9ecbae44987f83f2dae5711fc98a61b041852
csg <cgrigore@gmail.com>
2020-12-20 20:51:19 +0200

    clean the code

--- day-20/Program.cs
+++ day-20/Program.cs
@@ -18,92 +18,13 @@ namespace day_20
 
         public static void Solve()
         {
-            Solve2(File.ReadAllText("day-20.input.txt"));
+            Assert.Equal(new Tuple<long, long>(22878471088273, 1680), SolveBoth(File.ReadAllText("day-20.input.txt")));
         }
 
-        public class Pic
+        private static Tuple<long, long> SolveBoth(string input)
         {
-            public char[][] M { get; private set; }
-            public string[] Edges { get; private set; }
-            public int Number { get; private set; }
-
-            public Pic(int number, char[][] ma)
-            {
-                Number = number;
-                M = ma;
-                var n = String.Join("", ma[0]);
-                var e = String.Join("", ma.Select(line => line[line.Length - 1]));
-                var s = String.Join("", ma[ma.Length - 1]);
-                var w = String.Join("", ma.Select(line => line[0]));
-                // System.Console.WriteLine();
-                // System.Console.WriteLine(n);
-                // System.Console.WriteLine(e);
-                // System.Console.WriteLine(s);
-                // System.Console.WriteLine(w);
-                Edges = new[] { n, e, s, w };
-            }
-
-            public string Reverse(int edge)
-            {
-                return String.Join("", Edges[edge].Reverse());
-            }
-
-            public Pic Rotate()
-            {
-                var N = this.M.Length;
-                char[][] newM = new char[N][];
-                for (int i = 0; i < N; i++)
-                    newM[i] = new char[N];//it works in this situatio, since it's a square..
-                for (int i = 0; i < N; i++)
-                    for (int j = 0; j < N; j++)
-                        newM[j][N - i - 1] = M[i][j];
-
-                var ret = new Pic(this.Number, newM);
-                return ret;
-            }
-
-            public Pic Flip()
-            {
-                var N = this.M.Length;
-                //todo rotate here!
-                char[][] newM = new char[N][];
-                for (int i = 0; i < N; i++)
-                    newM[i] = new char[N];//it works in this situatio, since it's a square..
-                for (int i = 0; i < N; i++)
-                    for (int j = 0; j < N; j++)
-                        newM[j][i] = M[i][j];
-
-                var ret = new Pic(this.Number, newM);
-                return ret;
-            }
-
-            public Pic Copy()
-            {
-                var N = this.M.Length;
-                //todo rotate here!
-                char[][] newM = new char[N][];
-                for (int i = 0; i < N; i++)
-                    newM[i] = new char[N];//it works in this situatio, since it's a square..
-                for (int i = 0; i < N; i++)
-                    for (int j = 0; j < N; j++)
-                        newM[i][j] = M[i][j];
-
-                var ret = new Pic(this.Number, newM);
-                return ret;
-            }
-        }
-
-        public struct Piece
-        {
-            public int Number;
-            // public bool Flipped;
-            // public int Rotated;//0,1,2,3
-        }
-
-
-        private static long Solve2(string input)
-        {
-            long ret = 0L;
+            long retProblem2 = 0L;
+            long retProblem1 = 1L;
 
             var inputs = input.Split(Environment.NewLine + Environment.NewLine);
 
@@ -112,15 +33,15 @@ namespace day_20
             Read(inputs, pics);
 
             var len = (int)Math.Sqrt(pics.Count());
-            System.Console.WriteLine(len);
+            // System.Console.WriteLine(len);
 
             Pic[,] puzzle = new Pic[len, len];
             Dictionary<int, Pic> inner;
             var cnt = 0;
-            FindPossibleMatches(pics, out ret, out inner, puzzle, cnt++);
-            System.Console.WriteLine("sln1=" + ret);
+            FindPossibleMatches(pics, out retProblem1, out inner, puzzle, cnt++);
+
             while (inner.Count() > 0)
-                FindPossibleMatches(inner, out ret, out inner, puzzle, cnt++);
+                FindPossibleMatches(inner, out var prod, out inner, puzzle, cnt++);
 
             var noBordersImage = CombineAndRemoveBorders(puzzle);
             // Show(puzzle);
@@ -133,13 +54,11 @@ namespace day_20
 
             newPic = FillPattern(newPic);
 
-            ret = newPic.M.Sum(line => line.Where(c => c == '#').Count());
+            retProblem2 = newPic.M.Sum(line => line.Where(c => c == '#').Count());
 
 
-            // System.Console.WriteLine("\nimage with patterns");
-            // newPic.M.ShowMatrix();
-            Console.WriteLine(ret);
-            return ret;
+            Console.WriteLine($"{retProblem1} {retProblem2}");
+            return new Tuple<long, long>(retProblem1, retProblem2);
         }
 
         private static char[][] GetPattern()
@@ -147,7 +66,7 @@ namespace day_20
             var p =
                 "                  # " + Environment.NewLine +
                 "#    ##    ##    ###" + Environment.NewLine +
-                " #  #  #  #  #  #   ";// + Environment.NewLine;
+                " #  #  #  #  #  #   ";
             var pm = readmatrix(p.Split(Environment.NewLine));
             return pm;
         }
@@ -236,17 +155,9 @@ namespace day_20
             var lines = new List<String>();
             for (int i = 0; i < puzzle.GetLength(0); i++)
             {
-                // Console.Write(" | ");
-                for (int j = 0; j < puzzle.GetLength(1); j++)
-                {
-                    // Console.Write(puzzle[i, j]?.Number + " | ");
-                }
-                // System.Console.WriteLine();
-
                 for (int line = 1; line < puzzle[0, 0].M.Length - 1; line++)
                 {
                     var l = "";
-                    // Console.Write(" | ");
                     for (int j = 0; j < puzzle.GetLength(1); j++)
                     {
                         puzzle[i, j]?.M.ShowMatrix(line);
@@ -254,16 +165,9 @@ namespace day_20
 
                         for (var c = 1; c < puzzle[0, 0].M.Length - 1; c++)
                             l += p[line][c];
-
-                        // Console.Write(" | ");
-
-                        // Console.Write(puzzle[i, j]?.Number + ":");
                     }
                     lines.Add(l);
-                    // System.Console.WriteLine("");
                 }
-                // System.Console.WriteLine("");
-
             }
             return lines;
         }
@@ -322,11 +226,9 @@ namespace day_20
 
                 if (pic.Edges[edge] == current.Edges[comparingEdge])
                 {
-                    // System.Console.WriteLine($"match with {mpic.Number} on {r} and {mr}");
                     if (match)
                         throw new Exception("already match!");
                     return current;
-                    // Pic found = new Pic{Number=mpic.Numbe,r}
                 }
                 else if (pic.Edges[edge] == reverse.Edges[comparingEdge])
                 {
@@ -334,7 +236,6 @@ namespace day_20
                         throw new Exception("already match!");
 
                     return reverse;
-                    // System.Console.WriteLine($"match with {mpic.Number} on {r} and {mr}/R");
                 }
 
                 current = current.Rotate();
@@ -348,9 +249,10 @@ namespace day_20
             var N = puzzle.GetLength(0);
             var BEGIN = iteration;
             var END = N - iteration;
-            System.Console.WriteLine($"N={N} BEGIN={BEGIN} END={END}");
 
+            System.Console.WriteLine($"N={N} BEGIN={BEGIN} END={END}");
             System.Console.WriteLine($"possible matches iteration {iteration}");
+
             prod = 1;
             var corners = new Dictionary<int, Pic>();
             var edges = new Dictionary<int, Pic>();
@@ -364,18 +266,15 @@ namespace day_20
                 if (f != null)
                 {
                     puzzle[BEGIN, BEGIN] = f;
-                    // break;
                 }                //this happens only on the sample and not on the real input
                 else
                     throw new Exception("shouldnt happen!");
-                // puzzle[BEGIN,END-1]=pics.
                 return;
             }
 
             foreach (var (n, pic) in pics)
             {
                 links[pic.Number] = new List<int>();
-                // System.Console.WriteLine($"pic={pic.Number}");
                 var cnt = 0;
                 foreach (var (mn, mpic) in pics)
                     if (pic.Number != mpic.Number)
@@ -391,13 +290,10 @@ namespace day_20
                 {
                     corners.Add(pic.Number, pic);
                     prod *= pic.Number;
-                    // System.Console.WriteLine($"{pic.Number}");
                 }
                 else if (cnt == 3)
                 {
-                    // System.Console.WriteLine("EDGE FOUND" + pic.Number);
                     edges.Add(pic.Number, pic);
-
                 }
                 else if (cnt == 4)
                 {
@@ -406,31 +302,12 @@ namespace day_20
                 else
                     throw new Exception("shouldnt reach here!");
             }
-            System.Console.WriteLine($"c/e/i={corners.Count()}:{edges.Count()}:{inner.Count()}");
-
-            //fill the puzzle
-            // if (iteration == 0)
-            // {
-
-            //top,left corner
-            // if (iteration == 0)
-            // {
-            //     System.Console.WriteLine($"will take for corner {corners.ElementAt(0).Key}");
-            //     corners.ElementAt(0).Value.M.ShowMatrix();
-            //     puzzle[iteration, iteration] = corners.ElementAt(0).Value;
-            // }
-            // else
-            // {
-            //     var upper = puzzle[iteration - 1, iteration];
-
-
-
+            // System.Console.WriteLine($"c/e/i={corners.Count()}:{edges.Count()}:{inner.Count()}");
 
             var usedCorners = new List<int>();
             var usedEdges = new List<int>();
 
             //top left corner
-            // Show(corners, "corner");
             foreach (var (nctl, cTL) in corners)
                 if (!usedCorners.Contains(cTL.Number))
                 {
@@ -466,41 +343,28 @@ namespace day_20
                         usedCorners.Add(cTL.Number);
                         break;
                     }
-                    // Show(puzzle);
-                    // System.Console.WriteLine(puzzle[BEGIN, BEGIN].Number);
-
-                    // corners.Remove(puzzle[iteration, iteration].Number);
-
                 }
 
             //top edges
-            System.Console.WriteLine("top edges");
+            // System.Console.WriteLine("top edges");
             for (var column = BEGIN + 1; column < END - 1; column++)
             {
-                System.Console.WriteLine($"top edges column={column}");
-                // var found = false;
                 foreach (var (ne, edge) in edges)
                     if (!usedEdges.Contains(edge.Number))
                     {
-
                         var f = TranformToMatchEdge(puzzle[BEGIN, column - 1], edge, 1, out var cedge);
                         if (f != null)
                         {
                             puzzle[BEGIN, column] = f;
                             usedEdges.Add(edge.Number);
-                            // found = true;
                             break;
                         }
                     }
-                // if (found)
-                // break;
             }
 
-            // Show(puzzle);
-            System.Console.WriteLine("corner top right");
+            // System.Console.WriteLine("corner top right");
             //corner top right 
             foreach (var (nctr, cTR) in corners)
-
                 if (!usedCorners.Contains(cTR.Number))
                 {
                     var f = TranformToMatchEdge(puzzle[BEGIN, END - 1 - 1], cTR, 1, out var cedge);
@@ -511,15 +375,10 @@ namespace day_20
                         break;
                     }
                 }
-
-            // Show(corners);
-
-            // Show(puzzle);
             //right edges
-            System.Console.WriteLine("right edges");
+            // System.Console.WriteLine("right edges");
             for (var row = BEGIN + 1; row < END - 1; row++)
             {
-                // var found = false;
                 foreach (var (ne, edge) in edges)
                     if (!usedEdges.Contains(edge.Number))
                     {
@@ -528,18 +387,13 @@ namespace day_20
                         {
                             puzzle[row, END - 1] = f;
                             usedEdges.Add(edge.Number);
-                            // found = true;
                             break;
                         }
                     }
-                // if (found)
-                // break;
             }
 
-            // System.Console.WriteLine("try find bottom right");
-            // puzzle[END - 1 - 1, END - 1].M.ShowMatrix();
             //corner bottom right
-            System.Console.WriteLine("corner bottom right");
+            // System.Console.WriteLine("corner bottom right");
             foreach (var (ncbr, cBR) in corners)
                 if (!usedCorners.Contains(cBR.Number))
                 {
@@ -553,10 +407,9 @@ namespace day_20
                 }
 
             //left edges
-            System.Console.WriteLine("left edges");
+            // System.Console.WriteLine("left edges");
             for (var row = BEGIN + 1; row < END - 1; row++)
             {
-                // var found = false;
                 foreach (var (ne, edge) in edges)
                     if (!usedEdges.Contains(edge.Number))
                     {
@@ -565,12 +418,9 @@ namespace day_20
                         {
                             puzzle[row, BEGIN] = f;
                             usedEdges.Add(edge.Number);
-                            // found = true;
                             break;
                         }
                     }
-                // if (found)
-                // break;
             }
 
             //corner bottom left
@@ -589,28 +439,18 @@ namespace day_20
             //bottom edge
             for (var column = BEGIN + 1; column < END - 1; column++)
             {
-                // var found = false;
-
                 foreach (var (ne, edge) in edges)
                     if (!usedEdges.Contains(edge.Number))
                     {
-                        // System.Console.WriteLine();
-                        // System.Console.WriteLine($"COMPARE {edge.Number}");
-                        // puzzle[END - 1, column - 1].M.ShowMatrix();
-                        // System.Console.WriteLine();
-                        // edge.M.ShowMatrix();
 
                         var f = TranformToMatchEdge(puzzle[END - 1, column - 1], edge, 1, out var cedge);
                         if (f != null)
                         {
                             puzzle[END - 1, column] = f;
                             usedEdges.Add(edge.Number);
-                            // found = true;
                             break;
                         }
                     }
-                // if (found)
-                // break;
             }
         }
 
@@ -632,8 +472,6 @@ namespace day_20
             {
                 Console.Write(" | ");
                 for (int j = 0; j < puzzle.GetLength(1); j++)
-                // foreach (var rows in puzzle)
-                // foreach (var columns in rows)
                 {
                     Console.Write(puzzle[i, j]?.Number + " | ");
                 }
@@ -644,13 +482,9 @@ namespace day_20
 
                     Console.Write(" | ");
                     for (int j = 0; j < puzzle.GetLength(1); j++)
-                    // foreach (var rows in puzzle)
-                    // foreach (var columns in rows)
                     {
                         puzzle[i, j]?.M.ShowMatrix(line);
                         Console.Write(" | ");
-
-                        // Console.Write(puzzle[i, j]?.Number + ":");
                     }
                     System.Console.WriteLine("");
                 }
@@ -666,8 +500,6 @@ namespace day_20
                 (var fi, var la) = m.Split(Environment.NewLine);
                 var picno = int.Parse(fi.Replace("Tile ", "").Replace(":", ""));
                 var ma = readmatrix(la.ToArray());
-                // System.Console.WriteLine(picno);
-                // ma.ShowMatrix();
                 pics.Add(picno, new Pic(picno, ma));
             }
         }
@@ -681,58 +513,8 @@ namespace day_20
                 for (int j = 0; j < input[i].Length; j++)
                     matrix[i][j] = input[i][j];
             }
-            // matrix.ShowMatrix();
             return matrix;
         }
     }
 
-    internal struct NewStruct
-    {
-        public int i;
-        public int j;
-
-        public NewStruct(int i, int j)
-        {
-            this.i = i;
-            this.j = j;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return obj is NewStruct other &&
-                   i == other.i &&
-                   j == other.j;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(i, j);
-        }
-
-        public void Deconstruct(out int i, out int j)
-        {
-            i = this.i;
-            j = this.j;
-        }
-
-        public static implicit operator (int i, int j)(NewStruct value)
-        {
-            return (value.i, value.j);
-        }
-
-        public static implicit operator NewStruct((int i, int j) value)
-        {
-            return new NewStruct(value.i, value.j);
-        }
-    }
-}
-
-public static class Ex
-{
-    public static IEnumerable<IEnumerable<T>> DifferentCombinations<T>(this IEnumerable<T> elements, int k)
-    {
-        return k == 0 ? new[] { new T[0] } :
-          elements.SelectMany((e, i) =>
-            elements.Skip(i + 1).DifferentCombinations(k - 1).Select(c => (new[] { e }).Concat(c)));
-    }
 }
