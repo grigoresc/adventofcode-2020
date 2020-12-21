573f8361c8ea97389989ace117bdabd69a0f9452
csg <cgrigore@gmail.com>
2020-12-21 09:50:16 +0200

    day-21 prepare

---
+++ day-21/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Linq;
+using Xunit;
+using aoc.utils;
+using System.Collections.Generic;
+using System;
+using System.Text.RegularExpressions;
+
+namespace day_21
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Solve();
+        }
+
+        public static void Solve()
+        {
+        }
+
+    }
+
+}
